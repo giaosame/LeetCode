bc4dfb6961efb7183297b541ccbb7a5325110b07
giaosame <qzcqs@icloud.com>
2019-11-25 22:49:06 -0500

    add solution of 36

--- solutions1-50/problem36.cpp
+++ solutions1-50/problem36.cpp
@@ -1,21 +1,21 @@
 // 36. Valid Sudoku
 #include <vector>
-#include <unordered_set>
+#include <set>
 using namespace std;
 
 class Solution {
 public:
 	bool isValidSudoku(vector<vector<char>>& board) {
-		if (board.size() < 9 || board[0].size() < 9)
+		if (board.size() != 9 || board[0].size() != 9)
 			return false;
 
-		for (int i = 0; i < 9; i++)
+		for (int i = 0; i < board.size(); i++)
 		{
-			unordered_set<int> row;
-			unordered_set<int> col;
-			unordered_set<int> square;
+			set<int> row;
+			set<int> col;
+			set<int> square;
 
-			for (int j = 0; j < 9; j++)
+			for (int j = 0; j < board[0].size(); j++)
 			{
 				int row_idx = (i / 3) * 3;
 				int col_idx = (i % 3) * 3;
@@ -26,12 +26,14 @@ public:
 						return false;
 					row.insert(board[i][j]);
 				}
+
 				if (board[j][i] != '.')
 				{
 					if (col.find(board[j][i]) != col.end())
 						return false;
 					col.insert(board[j][i]);
 				}
+
 				if (board[row_idx + j / 3][col_idx + j % 3] != '.')
 				{
 					if (square.find(board[row_idx + j / 3][col_idx + j % 3]) != square.end())
